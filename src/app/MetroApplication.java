1dc4c726e57b51b8201196b38b5e7096f9561471
Pranit Zope <pranit.t.zope@gmail.com>
2023-10-21 13:44:59 +0530

    added graph and main app

---
+++ src/app/MetroApplication.java
@@ -0,0 +1,10 @@
+package app;
+
+import util.Graph;
+
+public class MetroApplication {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        System.out.println("APP HAS STARTED");
+    }
+}
